5fe640f0dedb1e84f281e36adba8dc09c642d014
Kuinox <github@kuinox.io>
2023-11-13 00:33:33 +0100

    Fixed hamburger menu.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,46 +17,49 @@ export interface Params {
 }
 
 export default function RootLayout({ children }: Params) {
+  // This is an hack to close the hamburger menu when the user clicks on a link.
+  // Basically next.js prevent default behavior when clicking on a link
+  // So we can't close it with pure html/css
+  // So we add a lightweight script that close it when the user navigate to another page
+  const scriptHTML = `
+  <script>
+  let checkbox = document.getElementById("top-bar-hamburger");
+  let originalPushState = history.pushState;
+  history.pushState = function(state, title, url) {
+      originalPushState.apply(this, arguments);
+      checkbox.checked = false;
+  };
+  </script>
+  `;
   return (
     <html>
       <body>
         <div className="app">
           <div className="top-bar">
             <div className="hamburger-wrapper">
-              <div className="hamburger-icon">
+              <input type="checkbox" id="top-bar-hamburger" className="hamburger-checkbox" />
+              <label htmlFor="top-bar-hamburger" className="hamburger-icon">
                 <div></div>
-              </div>
-              <div className="hamburger-close-icon">
-                <div></div>
-              </div>
-              <Link href="/" className="top-bar-link">
-                <Image
-                  src="/generated/Logo-Short.svg"
-                  className="top-bar-logo"
-                  alt="logo"
-                  height={50}
-                  width={55}
-                />
+              </label>
+              <Link href="/" className="top-bar-link close-hamberger-menu">
+                <Image src="/generated/Logo-Short.svg" className="top-bar-logo" alt="logo" height={50} width={55} />
               </Link>
               <div className="links">
-                <DracoButton buttonSize="medium" href={"/docs"} className="active-on-docs">
+                <DracoButton buttonSize="medium" href="/docs" className="active-on-docs close-hamberger-menu">
                   Documentation
                 </DracoButton>
                 {/*redirect to getting started for now*/}
-                <DracoButton buttonSize="medium" href={"/specs"} className="active-on-specs">
+                <DracoButton buttonSize="medium" href="/specs" className="active-on-specs close-hamberger-menu">
                   Specification
                 </DracoButton>
-                <DracoButton
-                  buttonSize="medium"
-                  href={"/community"}
-                  className="active-on-community"
-                >
+                <DracoButton buttonSize="medium" href="/community" className="active-on-community close-hamberger-menu">
                   Community
                 </DracoButton>
-                <DracoButton buttonSize="medium" href={"/blog"} className="active-on-blog">
+                <DracoButton buttonSize="medium" href="/blog" className="active-on-blog close-hamberger-menu">
                   Blog
                 </DracoButton>
               </div>
+              <div dangerouslySetInnerHTML={{ __html: scriptHTML }}></div>
             </div>
             <a href="https://github.com/Draco-lang/">
               <Image
--- src/components/DracoButton.tsx
+++ src/components/DracoButton.tsx
@@ -9,14 +9,15 @@ export default function DracoButton(props: {
   children: ReactNode;
   href: string;
   className?: string;
+  onClick?: () => void;
 }) {
-  const { buttonSize, children, className, href } = props;
+  const { buttonSize, children, className, href, onClick } = props;
   const buttonClassName = `draco-button ${
     className || ""
   } draco-button-${buttonSize}`;
 
   return (
-    <Link className={buttonClassName} href={href}>
+    <Link className={buttonClassName} href={href} onClick={onClick}>
       {children}
     </Link>
   );
